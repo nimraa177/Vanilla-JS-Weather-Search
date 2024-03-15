297767a2a621cc8c890c22e857aeb25a87446f40
Nimra Naseem Awan <58563145+nimraa177@users.noreply.github.com>
2024-03-15 12:31:47 -0500

    Organized HTML Code and Added comments in JS code

--- src/index.js
+++ src/index.js
@@ -1,60 +1,29 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windSpeedElement = document.querySelector("#wind-speed");
-  let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.time * 1000);
-  let iconElement = document.querySelector("#icon");
-
-  cityElement.innerHTML = response.data.city;
-  timeElement.innerHTML = formatDate(date);
-  descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
-}
-// setInterval(() => {
-//   timeElement.innerHTML = formatDate(date);
-// }, 1000);
-function formatDate(date) {
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
-
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  return `${day} ${hours}:${minutes}`;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
 }
 
+//function for search city
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  //call API and update UI
+  let apiKey = "0c309aff29364454354a4b94ab9ot995";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  axios.get(apiUrl).then(refreshWeather);
+  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather); //get the data and call function
 }
 
+//creation of handleSearchSubmit function
 function handleSearchSubmit(event) {
   event.preventDefault();
+  //value added in search saved in variable
   let searchInput = document.querySelector("#search-form-input");
-
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-searchCity("Paris");
+searchFormElement.addEventListener("submit", handleSearchSubmit); //on submit call handleSearchSubmit function
+//when page will load it will automatically call API
+searchCity("Islamabad");
